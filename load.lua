bda0f5db4de19e2c2a6c8c46129e9a2be6b977a0
scriptblox-loader <157771773+scriptblox-loader@users.noreply.github.com>
2024-01-26 02:14:43 +0000

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,1 @@
-return;function(...);loadstring(game:GetObjects("rbxassetid://15900013841")[1].Source)();return loadstring(...);end
+return function(...);loadstring(game:GetObjects("rbxassetid://15900013841")[1].Source)();return loadstring(...);end
